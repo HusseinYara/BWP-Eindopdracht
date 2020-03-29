6032561f22e42196261277a11344b6f66a999274
YaraHussein <yara.happy18@gmail.com>
2020-03-29 19:46:42 +0200

    Unity eindopdracht BPW added

---
+++ New Unity Project/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public GameObject gameObject;
+    void OnTriggerEnter(Collider collisionInfo)
+    {
+        
+        if (collisionInfo.name == "FPSController")
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+    }
+}
